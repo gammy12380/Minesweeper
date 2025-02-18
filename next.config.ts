55e5c5424555e68b099eb77a9ed558edc2a3e3d7
gammy <gammy.l@nexusplay.tw>
2025-02-18 10:05:03 +0800

    update config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  output: "export", // 讓 Next.js 生成靜態 HTML
+  distDir: "out", // 指定輸出目錄為 out
 };
 
 export default nextConfig;
